fa3fb1762a0c0a329d1072d7557f6e89c2d0a78c
PrantoshB <pbiswas1007@gmail.com>
2023-05-02 19:51:37 +0530

    Fix Linters Errors

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
-    belongs_to :user
-    has_and_belongs_to_many :payments, dependent: :destroy
+  belongs_to :user
+  has_and_belongs_to_many :payments, dependent: :destroy
 
-    validates :name, presence: true, length: { maximum: 50 }
-    validates :icon, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :icon, presence: true
 end
